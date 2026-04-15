e44544a4cb5b80caa1b9b507767b13f5bc058f1a
winhuirass-hue <winhuirass@gmail.com>
2026-04-15 15:13:42 +0300

    Update message for 32-bit and 64-bit architectures

--- app.c
+++ app.c
@@ -1,9 +1,14 @@
-/* app.c */
 long write(int, const void*, unsigned long);
 void _exit(int);
 
-int main(void) {
-    const char msg[] = "Hello from one-file minil\n";
-    write(1, msg, sizeof(msg)-1);
+int main(void)
+{
+#if defined(__x86_64__)
+    const char msg[] = "Hello from 64 bit minil\n";
+#else
+    const char msg[] = "Hello from 32 bit minil\n";
+#endif
+
+    write(1, msg, sizeof(msg) - 1);
     _exit(0);
 }
