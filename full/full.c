ab34d6a13d33474ddcc54cad4f759ed4816c0c33
winhuirass-hue <winhuirass@gmail.com>
2026-06-26 08:03:24 +0300

    Add overflow checking macros for addition, subtraction, and multiplication

--- full/full.c
+++ full/full.c
@@ -5,6 +5,10 @@
 extern "C" {
 #endif
 
+#define ckd_add(r,a,b) __builtin_add_overflow((a),(b),(r))
+#define ckd_sub(r,a,b) __builtin_sub_overflow((a),(b),(r))
+#define ckd_mul(r,a,b) __builtin_mul_overflow((a),(b),(r
+
 #define ALIGNOF(t) __alignof__(t)
 #define OFFSETOF(t,m) __builtin_offsetof(t,m)
 
